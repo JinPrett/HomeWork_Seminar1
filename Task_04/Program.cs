97e523e651d4def204af0d4ed90a2183373e53de
Evgeny Preobrazhenskii <jinprett@gmail.com>
2022-08-28 16:54:42 +0300

    Сделали задачу №4

---
+++ Task_04/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("Введите три целых числа:");
+int x = Convert.ToInt32(Console.ReadLine());
+int y = Convert.ToInt32(Console.ReadLine());
+int z = Convert.ToInt32(Console.ReadLine());
+int max = (int)MathF.Max(x,y);
+max = (int)MathF.Max(max,z);
+Console.WriteLine("Максимальное число из введённых = " + max);
