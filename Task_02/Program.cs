d9468d721391e52b514a01e774fcff5c31252e7b
Evgeny Preobrazhenskii <jinprett@gmail.com>
2022-08-28 16:18:00 +0300

    Сделали задачу № 2

---
+++ Task_02/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.WriteLine("Введите два целых числа:");
+int x = Convert.ToInt32(Console.ReadLine());
+int y = Convert.ToInt32(Console.ReadLine());
+if(x>y)
+{
+    Console.WriteLine("Первое число - большое, второе - меньшее!");
+}
+else if(x<y)
+{
+    Console.WriteLine("Второе число - большое, первое число - меньшее!");
+}
+else if(x==y)
+{
+    Console.WriteLine("Оба числа равны");
+}
